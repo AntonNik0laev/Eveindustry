c44e6f4507b91cfd4216b2d91c2f5635edf4c96b
Anton Nikolaev <re_b_e_l@mail.ru>
2020-12-14 16:25:35 +0300

    replaced telerik textbox with native input. added sdedata.bin to source control to siplify deployment and boostrapping. added cli project to create/update sdedata.bin file

---
+++ EveIndustry.UpdateData/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Extensions.DependencyInjection;
+using Eveindustry.Sde;
+using Eveindustry.Sde.Models.Config;
+using Eveindustry.Sde.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EveIndustry.UpdateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hb = new HostBuilder();
+
+            hb
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureAppConfiguration(c => c.AddInMemoryCollection(new Dictionary<string, string>()
+                {
+                    {"TypeInfoLoaderOptions:SdeBasePath", "d:/data/sde"},
+                }))
+                .ConfigureServices((context, services) =>
+            {
+                var config = context.Configuration;
+                services.AddOptions<TypeInfoLoaderOptions>().Bind(config.GetSection(nameof(TypeInfoLoaderOptions)));
+                services.TryAddSingleton<ISdeDataLoader, SdeDataLoader>();
+                services.AddHostedService<UpdateDataHostedService>();
+                services.AddLogging(c => c.AddConsole());
+            }).RunConsoleAsync();
+        }
+    }
+}
---
+++ EveIndustry.UpdateData/UpdateDataHostedService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Eveindustry.Sde;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EveIndustry.UpdateData
+{
+    public class UpdateDataHostedService: IHostedService
+    {
+        private readonly ILogger<UpdateDataHostedService> logger;
+        private readonly ISdeDataLoader loader;
+
+        public UpdateDataHostedService(ILogger<UpdateDataHostedService> logger, ISdeDataLoader loader)
+        {
+            this.logger = logger;
+            this.loader = loader;
+        }
+        
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var binPDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            var sdeFiles = Directory.GetFiles(binPDir, "*.bin");
+            foreach (var file in sdeFiles)
+            {
+                this.logger.LogInformation($"removing binary file: {file}");
+                File.Delete(file);
+            }
+
+            this.loader.Load().Wait();
+
+            if (File.Exists(Path.Combine(binPDir, "sdedata.bin")))
+            {
+                logger.LogInformation("sdedata.bin created");
+                Environment.Exit(0);
+                return Task.CompletedTask;
+                ;
+            }
+            
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
